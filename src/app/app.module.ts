eb3102e34b6e63f63f04f392a9a96f1075da928d
AndresSal <andres.salazar01@epn.edu.ec>
2018-06-12 15:56:10 -0500

    inicio - creación de un formulario en Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {FormsModule, NgForm} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -18,6 +19,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    FormsModule,
+    NgForm
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  nombre: 'Hola amigos de youtube';
+  password: 'aqui el password';
   constructor(public navCtrl: NavController) {
+    console.log('HOLAAAAAAAAAAAAAA');
+  }
 
+  validarFormulario(formulario) {
+    console.log(formulario);
   }
 
 }
